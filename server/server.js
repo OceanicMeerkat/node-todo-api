c8255f5fa2a08de571a1f0bc6855fec7fd7a3696
OceanicMeerkat <pai.suparanon@gmail.com>
2018-11-17 16:29:15 +0700

    test post todos rout

--- server/server.js
+++ server/server.js
@@ -20,6 +20,10 @@ app.post('/todos', (req,res) => {
     });
 });
 
-app.listen(3000, () => {
+
+app.listen(3000,() => {
     console.log("Server run on port 3000");
 });
+
+
+module.exports = {app};
