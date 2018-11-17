ad8966e453706969481e182b70a7bc3fb20f9d42
OceanicMeerkat <pai.suparanon@gmail.com>
2018-11-17 23:05:49 +0700

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -20,6 +20,13 @@ app.post('/todos', (req,res) => {
     });
 });
 
+app.get('/todos', (req,res) => {
+    Todo.find().then((todos)=>{
+        res.send({todos})
+    }, (err) => {
+        res.status(400).send(err);
+    })
+})
 
 app.listen(3000,() => {
     console.log("Server run on port 3000");
