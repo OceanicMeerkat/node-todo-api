f4934c0cd21477611eb445e9fd9344795e76faf9
OceanicMeerkat <pai.suparanon@gmail.com>
2018-11-17 13:58:30 +0700

    Set up server

--- server/server.js
+++ server/server.js
@@ -6,7 +6,19 @@ var {User} = require('./model/user');
 
 var app = express();
 
+app.use(bodyParser.json());
 
+app.post('/todos', (req,res) => {
+    var todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
 
 app.listen(3000, () => {
     console.log("Server run on port 3000");
