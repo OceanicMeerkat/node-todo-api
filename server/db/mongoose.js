5698ad1add73b050e8cb1954c6708d986038dccc
OceanicMeerkat <pai.suparanon@gmail.com>
2018-11-21 23:47:53 +0700

    Add conditional DB path

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,10 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect("mongodb://paichayon:P50040115CC#@ds157503.mlab.com:57503/todos" || 'mongodb://127.0.0.1:27017/TodoApp');
+var dbPath = process.env.PORT ? "mongodb://paichayon:P50040115CC#@ds157503.mlab.com:57503/todos" : "mongodb://127.0.0.1:27017/TodoApp"
+
+mongoose.connect(dbPath);
+console.log(`DB Location : ${dbPath}`);
 
 module.exports = { mongoose };
 
