f02c6bf9b3a5679ceb76452ad2fffe135d1885e0
OceanicMeerkat <pai.suparanon@gmail.com>
2018-11-25 11:31:20 +0700

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var {
+const _ = require('lodash');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const {
     mongoose
 } = require('./db/mongoose');
-var {
+const {
     Todo
 } = require('./model/todo');
-var {
+const {
     User
 } = require('./model/user');
 const {
@@ -80,6 +82,35 @@ app.delete('/todos/:id', (req, res) => {
     });
 
 });
+
+app.patch('/todos/:id', (req,res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text','completed']);
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+
+    if(_.isBoolean(body.completed) && body.completed){
+        body.completedAt =new Date().getTime();
+
+
+    }else{
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set : body}, {new: true}).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send(e);
+    })
+
+})
 
 app.listen(port, () => {
     console.log(`Server run on port ${port}`);
