525966ac43c944657ef71ba794be592f1499dd05
OceanicMeerkat <pai.suparanon@gmail.com>
2018-12-03 23:46:00 +0700

    Add POST /users/login

--- server/model/user.js
+++ server/model/user.js
@@ -79,6 +79,47 @@ UserSchema.statics.findByToken = function (token) {
     });
  }
 
+ UserSchema.statics.findByToken = function (token) { 
+     var User = this;
+     var decoded;
+
+     try{
+         decoded = jwt.verify(token, 'abc123');
+     } catch (e) {
+         return Promise.reject();
+     }
+
+
+     return User.findOne({
+        '_id': decoded._id,
+        'tokens.token' : token,
+        'tokens.access' : 'auth'
+     })
+
+  };
+
+  UserSchema.statics.findByCredentials = function (email,password) { 
+        var User = this;
+        return User.findOne({email}).then((user) => {
+            if (!user) {
+                return Promise.reject();
+            }
+
+            return new Promise((resolve, reject) => {
+                bcrypt.compare(password.toString(),user.password,(err,res) => {
+
+                    if(res){
+                        resolve(user);
+                    } else {
+                        reject();
+                    }
+                })
+            })
+            
+        })
+   };
+
+
  UserSchema.pre('save', function (next) { 
      var user = this;
 
